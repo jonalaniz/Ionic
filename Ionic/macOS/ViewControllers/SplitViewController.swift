209bc88f132048655c12a6ffd436b177a33d0c7b
Jon Alaniz <jonalaniz@gmail.com>
2025-04-22 13:57:02 -0500

    Updated UI with a hacked ass workaround

---
+++ Ionic/macOS/ViewControllers/SplitViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SplitViewController.swift
+//  Ionic
+//
+//  Created by Jon Alaniz on 4/21/25.
+//
+
+import Cocoa
+
+class SplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
