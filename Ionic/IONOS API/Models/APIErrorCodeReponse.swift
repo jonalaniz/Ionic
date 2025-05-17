cf3a185f232543ca0384a2258ebcd656bbe7ed13
Jon Alaniz <jonalaniz@gmail.com>
2025-05-17 10:31:08 -0500

    Added Error models

---
+++ Ionic/IONOS API/Models/APIErrorCodeReponse.swift
@@ -0,0 +1,36 @@
+//
+//  APIErrorCodeReponse.swift
+//  Ionic
+//
+//  Created by Jon Alaniz on 5/17/25.
+//
+
+import Foundation
+
+struct APIErrorCodeResponse: Decodable {
+    let code: String
+    let message: String?
+}
+
+struct APIErrorInvalidResponse: Decodable {
+    let code: String
+    let message: String?
+    let parameters: Parameters?
+    
+    struct Parameters: Decodable {
+        let errorRecord: Record
+        let inputRecord: Record?
+        let invalid: [String]?
+        let invalidFields: [String]?
+        let requiredFields: [String]?
+        
+        struct Record: Decodable {
+            let name: String
+            let disabled: Bool
+            let rootName: String
+            let content: String
+            let ttl: Int
+            let prio: Int
+        }
+    }
+}
---
+++ Ionic/IONOS API/Models/Enums/APIErrorCode.swift
@@ -0,0 +1,34 @@
+//
+//  APIErrorCode.swift
+//  Ionic
+//
+//  Created by Jon Alaniz on 5/17/25.
+//
+
+import Foundation
+
+/// Represents known HTTP status codes returned by the API and their associated meanings.
+///
+/// - `400` - Returns a `RecordError` based on detailed record validation failure.
+/// - `401`, `403`, `404` - Returns an `APIError` with a provided message from the server.
+/// - `429` - Indicates rate limiting; does not result in a thrown API error.
+/// - `500` - Indicates a server-side error; returns an `APIError` with no message.
+enum APIErrorCode: Int {
+    case badRequest = 400
+    case unauthorized = 401
+    case forbidden = 403
+    case recordNotFound = 404
+    case rateLimitExceeded = 429
+    case internalServerError = 500
+
+    var message: String {
+        switch self {
+        case .badRequest: return "Bad Request"
+        case .unauthorized: return "Unauthorized"
+        case .forbidden: return "Forbidden"
+        case .recordNotFound: return "Record Not Found"
+        case .rateLimitExceeded: return "Rate Limit Exceeded"
+        case .internalServerError: return "Internal Server Error"
+        }
+    }
+}
