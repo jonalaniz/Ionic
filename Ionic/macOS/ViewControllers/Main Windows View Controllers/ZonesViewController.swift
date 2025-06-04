535e17d6ac98b23d0652326f1d23e04afaf66282
Jon Alaniz <jonalaniz@gmail.com>
2025-06-04 04:52:25 -0500

    Moved NSTableViewDelegate to the ZoneManager

--- Ionic/macOS/ViewControllers/Main Windows View Controllers/ZonesViewController.swift
+++ Ionic/macOS/ViewControllers/Main Windows View Controllers/ZonesViewController.swift
@@ -15,7 +15,7 @@ class ZonesViewController: MainWindowViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         zoneTableView.dataSource = zoneManager
-        zoneTableView.delegate = self
+        zoneTableView.delegate = zoneManager
     }
 
     override func zonesReloaded() {
@@ -26,31 +26,5 @@ class ZonesViewController: MainWindowViewController {
             let indexSet = IndexSet(integer: selectedRow)
             zoneTableView.selectRowIndexes(indexSet, byExtendingSelection: false)
         }
-    }
-}
-
-extension ZonesViewController: NSTableViewDelegate {
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        let identifier = NSUserInterfaceItemIdentifier("ZoneCell")
-        guard let cell = tableView.makeView(
-            withIdentifier: identifier,
-            owner: self) as? NSTableCellView
-        else { return NSTableCellView() }
-
-        cell.textField?.stringValue = zoneManager.zones[row].name
-        cell.imageView?.image = image(for: zoneManager.zones[row].type)
-        cell.imageView?.toolTip = zoneManager.zones[row].type.description
-
-        return cell
-    }
-
-    func tableViewSelectionDidChange(_ notification: Notification) {
-        let selectedZone = zoneManager.zones[zoneTableView.selectedRow]
-        guard let zone = zoneManager.zoneDetails[selectedZone.id] else { return }
-        zoneManager.select(zone)
-    }
-
-    private func image(for type: ZoneType) -> NSImage? {
-        return NSImage(systemSymbolName: type.sfSymbolName, accessibilityDescription: "")
     }
 }
