4e500fbdd2b2be0786944e270bc0416e72ac2eca
Jon Alaniz <jonalaniz@gmail.com>
2025-06-07 23:42:38 -0500

    First zone is now automatically selected

--- Ionic/macOS/ViewControllers/Main Windows View Controllers/ZonesViewController.swift
+++ Ionic/macOS/ViewControllers/Main Windows View Controllers/ZonesViewController.swift
@@ -20,12 +20,25 @@ class ZonesViewController: MainWindowViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+        selectFirstRowIfNeeded()
+    }
+
+    // MARK: - Configuration
+    private func configureTableView() {
         zoneTableView.dataSource = zoneManager
         zoneTableView.delegate = zoneManager
     }
 
+    private func selectFirstRowIfNeeded() {
+        guard zoneTableView.numberOfRows > 0 else { return }
+        zoneTableView.selectRowIndexes(IndexSet(integer: 0), byExtendingSelection: false)
+    }
+
     // MARK: - Notification Handlers
 
+    /// Called when the zones are reloaded.
+    /// Attempts to reselect the previously selected row.
     override func zonesReloaded() {
         let selectedRow = zoneTableView.selectedRow
         zoneTableView.reloadData()
